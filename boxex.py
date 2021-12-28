d8992ab4b07ebb708c69a944a64321046dc56781
K.V.ROHIT <87985502+ROHIT1592007@users.noreply.github.com>
2021-12-28 12:52:36 +0530

    Add files via upload

---
+++ boxex.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+penup()
+goto(-150,100)
+pendown()
+speed(10)
+bgcolor("black")
+color("red")
+pensize(2)
+
+
+for i in range(8):
+    left(45)
+    for i in range(9):
+        forward(120)
+        right(90)
+
+
+hideturtle()
+exitonclick()
+
+turtle.done()
